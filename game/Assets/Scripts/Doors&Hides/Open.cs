23d7546858c34d58a1e626251efe09275f459fa8
WhyNotAG <golubkin1998@mail.ru>
2018-04-12 13:07:15 +0300

    some monipulation with cam and folders

---
+++ game/Assets/Scripts/Doors&Hides/Open.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Open : MonoBehaviour {
+	public int index;
+	public static GameObject player;
+	public static int way = 1;
+
+	void Start(){
+		player = GameObject.Find("Player");
+	}
+
+	public void OnMouseDown(){
+		if (transform.position.x + 7f >= player.transform.position.x
+		    && transform.position.x - 7f <= player.transform.position.x) {
+			if (this.tag == "Enter Doors") {
+				SceneManager.LoadScene (index);
+				way = -1;
+			} else if (this.tag == "Exit Doors") {
+				SceneManager.LoadScene (index);
+				way = 1;
+			}
+
+		}
+	}
+		
+}
